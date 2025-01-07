23688b2bac2f18c2aaca962258d196b9d447be95
BryanSigala <bryansigala@hotmail.com>
2025-01-06 22:51:32 -0600

    add: add text content and button to hero section

--- components/common/landing.jsx
+++ components/common/landing.jsx
@@ -1,19 +1,32 @@
-import Image from "next/image";
+import Image from "next/image"; 
+import { HeroSectionBtn } from "../ui/buttons";
+
+const content = {
+    heroSection: {
+        title: "Gestiona tus inventarios de forma colaborativa y eficiente", 
+        subtitle: "Empodera a tu equipo con una plataforma centralizada para un control total de tus recursos.", 
+        btn_text: "Comenzar gestión", 
+    }, 
+};
 
 export default function HeroSection(){
 
     return (
         <>
-            <section className="relative w-full h-screen bg-neutral-800/95 overflow-hidden">
+            <section className="relative flex items-center justify-center w-full h-screen bg-neutral-800/75 overflow-hidden">
                 <Image 
                     src='/images/backgrounds/heroSection_bg.jpg'
                     alt="ERP image"
                     fill
-                    className="object-cover absolute inset-0 opacity-50"
+                    className="object-cover absolute inset-0 -z-10"
                 /> 
-                <div className="flex flex-col">
-                    
-                </div>
+                <div className="w-3/5 gap-5 flex flex-col items-center text-gray-50 z-20">
+                    <h1 className="text-h3 font-extrabold text-center ">{content.heroSection.title}</h1>
+                    <p className="w-4/5 text-h5 font-semibold text-center">{content.heroSection.subtitle}</p>
+                    <HeroSectionBtn>
+                        {content.heroSection.btn_text}
+                    </HeroSectionBtn>
+                </div> 
             </section> 
         </>
     );
---
+++ components/ui/buttons.jsx
@@ -0,0 +1,19 @@
+import { motion } from 'motion/react'; 
+
+
+export function HeroSectionBtn({ children }){
+
+    return (
+        <>
+            <motion.button 
+                whileTap={{scale: 0.95}}
+                className="px-6 py-2 flex flex-row items-center gap-2 font-semibold flex-nowrap group bg-transparent text-inherit hover:text-cyan-300 hover:bg-neutral-800/50 transition-colors ease-in-out duration-200 text-h5 rounded-full">
+                {children}
+                <svg xmlns="http://www.w3.org/2000/svg" width="39.12" height="22" viewBox="0 0 16 9" className="transform group-hover:translate-x-2 transition-transform ease-in-out duration-200">
+                    <path fill="currentColor" d="M12.5 5h-9c-.28 0-.5-.22-.5-.5s.22-.5.5-.5h9c.28 0 .5.22.5.5s-.22.5-.5.5"/>
+                    <path fill="currentColor" d="M10 8.5a.47.47 0 0 1-.35-.15c-.2-.2-.2-.51 0-.71l3.15-3.15l-3.15-3.15c-.2-.2-.2-.51 0-.71s.51-.2.71 0l3.5 3.5c.2.2.2.51 0 .71l-3.5 3.5c-.1.1-.23.15-.35.15Z"/>
+                </svg>
+            </motion.button>
+        </>
+    );
+}
